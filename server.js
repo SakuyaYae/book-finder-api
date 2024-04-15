1609f2edf1f091832833d3455e233b47f3b9d428
SakuyaYae <darkreimu@outlook.com>
2024-04-15 13:52:00 +0200

    Create server.js

---
+++ server.js
@@ -0,0 +1,15 @@
+import express from "express"
+import mongoose from "mongoose"
+import router from "./router.js"
+const server = express()
+
+const port = 4444
+
+server.use(express.json())
+const conectionString = "";
+
+mongoose.connect(conectionString);
+
+router(server, mongoose);
+
+server.listen(port, () => console.log(`Running on port http://localhost:${port}`))
