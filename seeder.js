c73fd2c02ba79dac142ef14785262e276afe1c67
SakuyaYae <darkreimu@outlook.com>
2024-04-15 14:57:00 +0200

    Update seeder.js

--- seeder.js
+++ seeder.js
@@ -1,2 +1,25 @@
 import { faker } from '@faker-js/faker';
 import mongoose from 'mongoose';
+import Book from './models/book.js';
+const conectionString = "";
+
+async function seedDb() {
+  mongoose.connect(conectionString);
+  const seededData = generateBooks(5);
+}
+
+
+async function generateBooks(bookAmount) {
+  const bookList = [];
+  for (var i = 0; i < bookAmount; i++) {
+    const book = new Book({
+      "title": "",
+      "author": "",
+      "genre": "",
+      "releaseDate": ""
+    });
+    bookList.push(book);
+  }
+  return bookList;
+}
+seedDb();
