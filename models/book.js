bbb63563f39bf92d22c37e1eb63935fe992702e1
SakuyaYae <darkreimu@outlook.com>
2024-04-15 13:51:51 +0200

    Create book.js

---
+++ models/book.js
@@ -0,0 +1,12 @@
+import mongoose from "mongoose";
+
+const bookSchema = new mongoose.Schema({
+  "title": String,
+  "author": String,
+  "genre": String,
+  "releaseDate": String
+});
+
+const Book = mongoose.model("books", bookSchema);
+
+export default Book;
