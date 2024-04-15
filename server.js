3e069522765053f6fba8148f934c0628ff13c236
SakuyaYae <darkreimu@outlook.com>
2024-04-15 14:44:41 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
-import express from "express"
-import mongoose from "mongoose"
-import router from "./router.js"
-const server = express()
+import express from "express";
+import mongoose from "mongoose";
+import router from "./router.js";
+const server = express();
 
-const port = 4444
+const port = 4444;
 
-server.use(express.json())
+server.use(express.json());
 const conectionString = "";
 
 mongoose.connect(conectionString);
 
 router(server, mongoose);
 
-server.listen(port, () => console.log(`Running on port http://localhost:${port}`))
+server.listen(port, () => console.log(`Running on port http://localhost:${port}`));
