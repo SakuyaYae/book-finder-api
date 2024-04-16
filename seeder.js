4c3ce016d047763d2cf5706545f17b183f0b141e
SakuyaYae <darkreimu@outlook.com>
2024-04-16 11:21:28 +0200

    Update seeder.js
    added data to bookObj

--- seeder.js
+++ seeder.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import Book from './models/book.js';
 import "dotenv/config";
 const conectionString = process.env.conectionString;
+const genreList = ["action", "adventure", "horror", "fantasy", "romance", "si-fi", "hitorical"];
 
 async function seedDb() {
   mongoose.connect(conectionString);
@@ -15,10 +16,10 @@ async function generateBooks(bookAmount) {
   const bookList = [];
   for (var i = 0; i < bookAmount; i++) {
     const book = new Book({
-      "title": "",
-      "author": "",
-      "genre": "",
-      "releaseDate": ""
+      "title": "History of " + faker.location.country(),
+      "author": faker.person.fullName(),
+      "genre": genreList[faker.number.int(6)],
+      "releaseDate": faker.date.past()
     });
     bookList.push(book);
   }
