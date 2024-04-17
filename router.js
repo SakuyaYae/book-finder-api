b0e24d86e89cf4b8645045d9927862965d59cbd1
SakuyaYae <darkreimu@outlook.com>
2024-04-17 14:23:44 +0200

    added sortng rout

--- router.js
+++ router.js
@@ -1,9 +1,11 @@
 import books from "./api/bookEndPoints.js";
 import bookFilter from "./api/bookFilter.js";
+import bookSorting from "./api/bookSorting.js";
 import databaseCleaner from "./api/databaseCleaner.js";
 
 export default function (server) {
   books(server);
   databaseCleaner(server);
   bookFilter(server);
+  bookSorting(server);
 }
