eb853f352effe11ccb0924a05026680de700ccd1
SakuyaYae <darkreimu@outlook.com>
2024-04-17 14:23:53 +0200

    added sorting

--- api/bookSorting.js
+++ api/bookSorting.js
@@ -1,15 +1,42 @@
 import Book from "./../models/book.js";
 
-export default function (server, mongoose) {
-  server.get("/api/books/:id", async (req, res) => {
+export default function (server) {
+  server.get("/api/books/sort/title", async (req, res) => {
     try {
-      const selectedBook = await Book.findById(req.params.id).populate('manga');
-
-      if (!selectedBook) {
-        res.status(404).json({ "msg": "Not found" })
+      const booksSortedByTitle = await Book.find().sort({ title: 1 });
+      if (booksSortedByTitle.length < 1) {
+        res.status(204).json({ "msg": "no content" });
+      } else {
+        res.status(200).json([{ "msg": "OK" }, booksSortedByTitle]);
       }
-
-      res.status(200).json([{ "msg": "OK" }, selectedBook]);
+    }
+    catch (error) {
+      console.error(error);
+      res.status(500).json([{ "msg": "error" }, error]);
+    }
+  });
+  server.get("/api/books/sort/author", async (req, res) => {
+    try {
+      const booksSortedByAuthor = await Book.find().sort({ author: 1 });
+      if (booksSortedByAuthor.length < 1) {
+        res.status(204).json({ "msg": "no content" });
+      } else {
+        res.status(200).json([{ "msg": "OK" }, booksSortedByAuthor]);
+      }
+    }
+    catch (error) {
+      console.error(error);
+      res.status(500).json([{ "msg": "error" }, error]);
+    }
+  });
+  server.get("/api/books/sort/genre", async (req, res) => {
+    try {
+      const booksSortedByGenre = await Book.find().sort({ genre: 1 });
+      if (booksSortedByGenre.length < 1) {
+        res.status(204).json({ "msg": "no content" });
+      } else {
+        res.status(200).json([{ "msg": "OK" }, booksSortedByGenre]);
+      }
     }
     catch (error) {
       console.error(error);
