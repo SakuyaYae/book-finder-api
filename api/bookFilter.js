b0896240089aedca8347e94deceab23c096b6fce
SakuyaYae <darkreimu@outlook.com>
2024-04-17 10:52:35 +0200

    Update bookFilter.js
    added a fix for a fatal error

--- api/bookFilter.js
+++ api/bookFilter.js
@@ -1,15 +1,19 @@
 import Book from "./../models/book.js";
 
 export default function (server) {
-  server.get("/api/books/:title", async (req, res) => {
+  server.get("/api/filter/:title", async (req, res) => {
     try {
       const booksByName = await Book.find({ title: req.params.title });
 
       if (!booksByName) {
         res.status(404).json({ "msg": "Not found" })
       }
-
-      res.status(200).json([{ "msg": "OK" }, booksByName]);
+      else if (booksByName.length < 1) {
+        res.status(204).json({ "msg": "no content" })
+      }
+      else {
+        res.status(200).json([{ "msg": "OK" }, booksByName]);
+      }
     }
     catch (error) {
       console.error(error);
@@ -17,15 +21,19 @@ export default function (server) {
     }
   });
 
-  server.get("/api/books/:author", async (req, res) => {
+  server.get("/api/filter/:author", async (req, res) => {
     try {
       const booksByAuthor = await Book.find({ author: req.params.author });
 
       if (!booksByAuthor) {
         res.status(404).json({ "msg": "Not found" })
       }
-
-      res.status(200).json([{ "msg": "OK" }, booksByAuthor]);
+      else if (booksByAuthor.length < 1) {
+        res.status(204).json({ "msg": "no content" })
+      }
+      else {
+        res.status(200).json([{ "msg": "OK" }, booksByName]);
+      }
     }
     catch (error) {
       console.error(error);
@@ -33,15 +41,19 @@ export default function (server) {
     }
   });
 
-  server.get("/api/books/:genre", async (req, res) => {
+  server.get("/api/filter/:genre", async (req, res) => {
     try {
       const booksByGenre = await Book.find({ author: req.params.genre });
 
       if (!booksByGenre) {
         res.status(404).json({ "msg": "Not found" })
       }
-
-      res.status(200).json([{ "msg": "OK" }, booksByGenre]);
+      else if (booksByGenre.length < 1) {
+        res.status(204).json({ "msg": "no content" })
+      }
+      else {
+        res.status(200).json([{ "msg": "OK" }, booksByName]);
+      }
     }
     catch (error) {
       console.error(error);
