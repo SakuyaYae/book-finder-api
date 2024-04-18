dad812cbfcd7720f177c1e262fd4e68288052569
SakuyaYae <darkreimu@outlook.com>
2024-04-18 15:17:39 +0200

    Update databaseCleaner.js
    added saifty

--- api/databaseCleaner.js
+++ api/databaseCleaner.js
@@ -3,8 +3,12 @@ export default function (server, mongoose) {
   server.delete("/api/cleaner", async (req, res) => {
     try {
       const cleaner = await Book.deleteMany();
-      if (!cleaner) { res.status(404).json({ "msg": "Not found" }) }
-      res.status(204).json([{ "msg": "cleaning done" }]);
+      if (!cleaner) {
+        res.status(404).json({ "msg": "Not found" })
+      }
+      else {
+        res.status(204).json([{ "msg": "cleaning done" }]);
+      }
     }
     catch (error) {
       console.error(error);
