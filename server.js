9535f865ff7ef6e526bcd951585b11157248947c
SakuyaYae <darkreimu@outlook.com>
2024-04-24 10:58:55 +0200

    rate limiter added

--- server.js
+++ server.js
@@ -1,12 +1,18 @@
 import express from "express";
 import mongoose from "mongoose";
 import router from "./router.js";
+import rateLimit from "express-rate-limit";
 import "dotenv/config";
 const server = express();
-
+const rateLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  limit: 500,
+  message: "request limit reached"
+});
 const port = 4444;
 
 server.use(express.json());
+server.use("/api", rateLimiter);
 const conectionString = process.env.conectionString;
 
 mongoose.connect(conectionString);
