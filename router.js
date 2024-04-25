35f16544d343d59a2663f257d2c391f8acda0bb8
SakuyaYae <darkreimu@outlook.com>
2024-04-25 12:02:56 +0200

    Update router.js
    added rout

--- router.js
+++ router.js
@@ -1,11 +1,13 @@
 import books from "./api/bookEndPoints.js";
 import bookFilter from "./api/bookFilter.js";
 import bookSorting from "./api/bookSorting.js";
+import conDbTest from "./api/conDbTest.js";
 import databaseCleaner from "./api/databaseCleaner.js";
 
-export default function (server) {
-  books(server);
-  databaseCleaner(server);
-  bookFilter(server);
-  bookSorting(server);
+export default function (server, mongoose) {
+  books(server, mongoose);
+  databaseCleaner(server, mongoose);
+  bookFilter(server, mongoose);
+  bookSorting(server, mongoose);
+  conDbTest(server, mongoose);
 }
